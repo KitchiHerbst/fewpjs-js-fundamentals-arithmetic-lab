e3c22bd4ece06f42d6685da5f2757f201f0fd6f0
Triston <tristonherbst97@gmail.com>
2021-04-14 16:16:18 -0500

    commit tree 128515996c24e389fa7fbe0ba0d03df35caf90bd

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31 
+let num2 = 2
+
+let num3 = 10
+let num4 = 6
+
+let num5 = 20 
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10)
+
+const mod = num3 % num4
+
+const max = Math.max(num2,num3,num4,num5)
+
